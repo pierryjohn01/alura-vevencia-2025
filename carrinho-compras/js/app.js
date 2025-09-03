0ee91d47a7b7484597c04a16e372fdb459fa022f
pierryjohn01 <johnpierry18@gmail.com>
2025-09-03 10:21:18 -0300

    Add files via upload

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,33 @@
+let totalGeral;
+limpar();
+
+
+function adicionar(){
+
+// recuperar
+let Produto = document.getElementById('produto').value;
+let nomeProduto = Produto.split('-') [0];
+let valorUnitario = Produto.split('R$')[1];
+let quantidade = document.getElementById('quantidade').value;
+
+
+// calcular preço
+let preco = quantidade * valorUnitario;
+let carrinho = document.getElementById('lista-produtos');
+carrinho.innerHTML =  carrinho.innerHTML +  `<section class="carrinho__produtos__produto">
+<span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">R$ ${valorUnitario}</span>
+</section>`;
+
+// total
+totalGeral = totalGeral + preco;
+let campoTotal = document.getElementById('valor-total');
+campoTotal.textContent = ` R$ ${totalGeral}`;
+document.getElementById('quantidade').value = 0;
+}
+
+function limpar() {
+totalGeral = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').innerHTML = '';
+
+}
